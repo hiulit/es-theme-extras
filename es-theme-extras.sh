c0eb5abdb9cefe17b7a5bca50f438a4a71b65b18
hiulit <hiulit@gmail.com>
2017-10-05 18:01:00 +0200

    Commented some echoes

--- es-theme-extras.sh
+++ es-theme-extras.sh
@@ -142,7 +142,7 @@ function install_splashscreen() {
     check_theme
     if [[ ! -d "$splashscreens_path" ]]; then
         mkdir -p $splashscreens_path
-        echo "Splashscreens folder created successfully!"
+        # echo "Splashscreens folder created successfully!"
     fi
     choose_splashscreen
 }
@@ -171,7 +171,7 @@ function choose_splashscreen() {
                 fi
                     rm -f $splashscreens_path/*
                     cp $themes_path/$theme/$splashscreen $splashscreens_path
-                    echo "$splashscreen installed successfully!"
+                    # echo "$splashscreen installed successfully!"
                 ;;
         esac
     fi
